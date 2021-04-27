ad816436cb77f009ea2f2fa13a8e91d9c30873b7
Ketan Jadhav <ketan.jadhav@successive.tech>
2021-04-27 17:37:27 +0530

    fix: permission.js

--- extra/index.js
+++ extra/index.js
@@ -31,7 +31,7 @@ for (let i = 2; i < process.argv.length; i++) {
     process.stdout.write("Enter number between 2 to 10");
   }
 }
-console.log("Permission access: ",hasPermission("getUsers", "head-trainer", "all"));
+console.log("Permission access: ",hasPermission("getUsers", "head-trainer", "read"));
 
 ValidateUsers(users);
 
--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -1,21 +1,8 @@
-import {permissions} from './../constants';
+import { permissions } from "./../constants";
 
 const hasPermission = (moduleName, role, permission) => {
-  for (const [key] of Object.entries(permissions)) {
-    if (moduleName === key) {
-      for (const [keys, values] of Object.entries(permissions.getUsers)) {
-        if (keys == permission) {
-          for (const [keys, valueR] of Object.entries(values)) {
-            if (valueR == role) {
-              return true;
-            }
-          }
-        }
-      }
-    }
-  }
-
-  return false;
+  const module = permissions[moduleName];
+  return module[permission].includes(role) || module["all"].includes(role);
 };
 
 export default hasPermission;
