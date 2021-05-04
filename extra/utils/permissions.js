d07b5192a2acb7b98ffbb09915044d2cd12f0339
Ketan Jadhav <ketan.jadhav@successive.tech>
2021-05-04 19:07:14 +0530

    fix: permissions.js

--- extra/utils/permissions.js
+++ extra/utils/permissions.js
@@ -2,10 +2,14 @@ import { permissions } from "./../constants";
 
 const hasPermission = (moduleName, role, permission) => {
   const module = permissions[moduleName];
-  if(!module){
+  if (!module) {
     return false;
   }
-  return module[permission].includes(role) || module["all"].includes(role);
+  if (!module[permission]) {
+    return false;
+  } else {
+    return module[permission].includes(role) || module["all"].includes(role);
+  }
 };
 
 export default hasPermission;
